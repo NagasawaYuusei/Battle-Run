63a59afdc1f55eafa57fbea209a0d2ec8bf37729
NagasawaYusei <curga3013@gmail.com>
2021-10-13 20:22:05 +0900

    プレイヤーコントローラー途中

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody m_rb;
-    [SerializeField] float m_moveSpeed;
-    [SerializeField] float m_mouseSensitivity = 100f;
-    [SerializeField] GameObject m_camera;
-    float m_mouseX;
-    float m_mouseY;
+    Rigidbody m_rb; //Rigidbody
+    float m_mouseX; //マウスの横入力の箱 
+    float m_mouseY; // マウスの縦入力の箱
+
+    [Header("設定項目")]
+    [SerializeField] float m_moveSpeed; //プレイヤーのスピード
+    [SerializeField] float m_dropSpeed;
+    [SerializeField] float m_mouseSensitivity = 100f; //マウス感度
+    [Space(1)]
+
+    [Header("スクリプトで使うもの")]
+    [SerializeField] GameObject m_camera; //カメラオブジェクト
 
     void Start()
     {
@@ -30,21 +34,35 @@ public class PlayerController : MonoBehaviour
     {
         float inputMouseX = Input.GetAxis("Mouse X");
         float inputMouseY = Input.GetAxis("Mouse Y");
-        m_mouseX += inputMouseX;
-        m_mouseY += inputMouseY;
-        Vector3 mouse = new Vector3(-m_mouseY, m_mouseX,0);
-        m_camera.transform.localRotation = Quaternion.Euler(mouse);
+        m_mouseX += inputMouseX * m_mouseSensitivity;
+        m_mouseY += inputMouseY * m_mouseSensitivity;
+        if(m_mouseY >= 90)
+        {
+            m_mouseY = 90;
+        }
+        if(m_mouseY <= -90)
+        {
+            m_mouseY = -90;
+        }
+        m_camera.transform.localRotation = Quaternion.Euler(-m_mouseY, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, m_mouseX, 0);
     }
     void FixedUpdate()
     {
+        Status();
         Move();
+    }
+
+    void Status()
+    {
+        //m_rb.velocity.y -=
+
     }
 
     void Move()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        Vector3 dir = new Vector3(h, m_rb.velocity.y, v);
-        m_rb.velocity = dir.normalized * m_moveSpeed;
+        m_rb.velocity = (transform.forward * v * m_moveSpeed) + (transform.right * h * m_moveSpeed);
     }
 }
