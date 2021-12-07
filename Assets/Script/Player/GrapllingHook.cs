6c0e8c90ceb4cc25140028917e21a9fcd9744725
NagasawaYusei <curga3013@gmail.com>
2021-12-07 12:20:34 +0900

    セーブ

--- Assets/Script/Player/GrapllingHook.cs
+++ Assets/Script/Player/GrapllingHook.cs
@@ -11,7 +11,7 @@ public class GrapllingHook : MonoBehaviour
     [SerializeField] float m_maxDistance = 100;
     [SerializeField] float m_grappleSpeed;
 
-    PlayerJoint m_joint;
+    SpringJoint m_joint;
     float m_distanceFromPoint;
 
     void Start()
@@ -44,6 +44,11 @@ public class GrapllingHook : MonoBehaviour
         //{
         //    StopGrapple();
         //}
+        if(m_joint)
+        {
+            m_joint.maxDistance -= m_grappleSpeed;
+            m_joint.minDistance -= m_grappleSpeed;
+        }
     }
 
     void StartGrapple()
@@ -52,26 +57,26 @@ public class GrapllingHook : MonoBehaviour
         if (Physics.Raycast(m_camera.position, m_camera.forward, out hit, m_maxDistance, m_whatIsGrappleable))
         {
             m_grapplePoint = hit.point;
-            //m_joint = m_player.gameObject.AddComponent<SpringJoint>();
+            m_joint = m_player.gameObject.AddComponent<SpringJoint>();
 
-            //m_joint.autoConfigureConnectedAnchor = false;
-            //m_joint.connectedAnchor = m_grapplePoint;
+            m_joint.autoConfigureConnectedAnchor = false;
+            m_joint.connectedAnchor = m_grapplePoint;
 
-            //m_distanceFromPoint = Vector3.Distance(m_player.position, m_grapplePoint);
+            m_distanceFromPoint = Vector3.Distance(m_player.position, m_grapplePoint);
 
-            //m_joint.maxDistance = m_distanceFromPoint;
-            //m_joint.minDistance = m_distanceFromPoint;
+            m_joint.maxDistance = m_distanceFromPoint;
+            m_joint.minDistance = m_distanceFromPoint;
 
             //m_joint.spring = 4.5f;
             //m_joint.damper = 7f;
             //m_joint.massScale = 4.5f;
 
-            m_joint = m_player.gameObject.AddComponent<PlayerJoint>();
-            m_joint.m_connectedAnchor = m_grapplePoint;
+            //m_joint = m_player.gameObject.AddComponent<PlayerJoint>();
+            //m_joint.m_connectedAnchor = m_grapplePoint;
 
             m_lr.positionCount = 2;
 
-            m_player.GetComponent<PlayerController>().enabled = false;
+            //m_player.GetComponent<PlayerController>().enabled = false;
         }
         Debug.Log("Start");
     }
@@ -80,7 +85,7 @@ public class GrapllingHook : MonoBehaviour
     {
         m_lr.positionCount = 0;
         Destroy(m_joint);
-        m_player.GetComponent<PlayerController>().enabled = true;
+        //m_player.GetComponent<PlayerController>().enabled = true;
     }
 
     void LateUpdate()
