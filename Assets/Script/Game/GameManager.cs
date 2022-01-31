d86a4b4ae8a032708053b1d83f072f459aeeb36b
NagasawaYusei <curga3013@gmail.com>
2022-01-31 20:17:06 +0900

    stage途中

---
+++ Assets/Script/Game/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    protected override bool dontDestroyOnLoad { get { return true; } }
+
+
+
+    public void A()
+    {
+        
+    }
+}
